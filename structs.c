e15a089c9914d4dc5ce5cb553f3d4df9c042aaec
mishkadoma <mixandoma@ya.ru>
2017-03-13 17:15:46 +0500

    playing with structs

---
+++ structs.c
@@ -0,0 +1,10 @@
+#include "structs.h"
+
+struct worker first;
+
+first.age = 24;
+first.name = "Arthour";
+first.weight = 74;
+first.ibs = 2538891927293;
+
+printf("first.age: %i\n", first.age);
---
+++ structs.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+
+struct worker
+{
+  int age;
+  char* name;
+  int weight;
+  double ibs;
+};
