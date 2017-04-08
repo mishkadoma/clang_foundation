dcc2be70c006419ec16921dc79066494ab8f88ac
mishkadoma <mixandoma@ya.ru>
2017-04-08 08:33:01 +0500

    my_name added

--- arraytest.c
+++ arraytest.c
@@ -1,11 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
-    int x[4]={0, 2, 3, 4};
+    int x[] = { 0, 2, 3, 4 };
+    char my_name[] = { 'M', 'I', 'K', 'H', 'A', 'I', 'L'};
+
     for (int i=0; i<4; i++)
     {
-        printf("%i\n", x[i]);
+      printf("%i\n", x[i]);
     }
+
+    for (int i = 0; i < (sizeof(my_name)); i++)
+    {
+      printf("%c\n", my_name[i]);
+    }
+    return 0;
 }
