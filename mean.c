fbc3934aa38099d28d72461f6b31e062f375298e
Mickhail <mixandoma@ya.ru>
2017-04-09 19:07:50 +0000

    test

---
+++ mean.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cs50.h>
+
+#define N 5
+
+float the_mean(int *x);
+
+int main(void)
+{
+    int numbers[N];
+    
+    for (int x = 0; x < N; x++)
+    {
+        printf("Enter your number:");
+        numbers[x] = get_int();
+    }
+    
+    printf("Average: %.2f\n", the_mean(numbers));
+    
+    return 0;
+}
+
+float the_mean(int *x)
+{
+    int result = 0;
+    for (int n = 0; n < N; n++)
+    {
+        result += x[n];
+    }
+    
+    return ((float) result/N);
+}
