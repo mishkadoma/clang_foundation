51f68e6d23dc02222a3972322f82d7b8432fc87f
mishkadoma <mixandoma@ya.ru>
2017-05-03 19:21:08 +0500

    added contains.c

---
+++ contains.c
@@ -0,0 +1,64 @@
+#include <cs50.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 5
+
+typedef struct node
+{
+    // the value to store in this node
+    int n;
+
+    // the link to the next node in the list
+    struct node* next;
+}
+node;
+
+node* head = NULL;
+
+bool contains(int i)
+{
+    for (node *pointer = head; pointer != NULL; pointer = pointer->next)
+    {
+        if (pointer->n == i)
+            return true;
+    }
+    return false;
+}
+
+int main(int argc, char* argv[])
+{
+    // create linked list
+    for (int i = SIZE; i > 0; i--)
+    {
+        node* new = malloc(sizeof(node));
+
+        if (new == NULL)
+        {
+            exit(1);
+        }
+        new->n = i;
+        new->next = head;
+        head = new;
+    }
+
+    printf("What value are you looking for? ");
+    int i = GetInt();
+
+    if (contains(i))
+    {
+        printf("Found it! The list contains ");
+    }
+    else
+    {
+        printf("Sorry, the list only contains ");
+    }
+    for (node*  ptr = head; ptr != NULL; ptr = ptr->next)
+    {
+        printf("%i ", ptr->n);
+    }
+    printf("\n");
+
+    return 0;
+}
